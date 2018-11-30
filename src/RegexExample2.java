607f877c54a204b71798158ec83a6a8b52ec4e00
chuchumj <janeibodje@gmail.com>
2018-11-30 00:26:59 -0500

    Regex Example2 // password validation

--- src/RegexExample2.java
+++ src/RegexExample2.java
@@ -1,13 +1,14 @@
+//Program for validation of password for a certain format specified by the Regex
 import java.util.regex.*;
 import java.util.Scanner;
 public class RegexExample2 {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        Pattern p3 = Pattern.compile("[a-zA-Z]{4}@{1}.{1}\\d");
         System.out.println("Enter your email");
         scan.nextLine();
         System.out.println("Choose a password");
-        System.out.println("Password should start with any 4 letters, followed by one \"@\" symbol,followed by one \".\" symbol, then any digit from 0-9");
+        System.out.println("Password should start with any 4 letters, followed by one \"@\" symbol,followed by one \".\", then any digit from 0-9");
+        Pattern p3 = Pattern.compile("[a-zA-Z]{4}@{1}.{1}\\d");
         Matcher m3 = p3.matcher(scan.nextLine());
         if (m3.matches()) {
         System.out.println(" Valid password selected");
--- src/RegexProject.java
+++ src/RegexProject.java
@@ -5,13 +5,13 @@ public class RegexProject {
         Matcher m = p.matcher("te a9999999"); // * : is it the end of the line?
         System.out.println(m.matches());
 
-        Pattern p1 = Pattern.compile("^..a$"); // ^ : is it the beginning of the line?
-        Matcher m1 = p1.matcher("tea");     // $: is it the end of the line?
-        System.out.println(m1.matches());
+       // Pattern p1 = Pattern.compile("^..a$"); // ^ : is it the beginning of the line?
+       // Matcher m1 = p1.matcher("tea");     // $: is it the end of the line?
+        System.out.println(Pattern.compile("^..a$").matcher("tea").matches());
 
-        Pattern p2 = Pattern.compile("....?...\\s.....s+io?n"); //+ matches one or more of the previous element
-        Matcher m2 = p2.matcher("Regular Expression");      // ?: matches 0 or more occurence of he previous element
-        System.out.println(m2.matches()); // true
+        //Pattern p2 = Pattern.compile("....?...\\s.....s+io?n"); //+ matches one or more of the previous element
+       // Matcher m2 = p2.matcher("Regular Expression");      // ?: matches 0 or more occurence of he previous element
+        System.out.println(Pattern.matches("....?...\\s.....s+io?n","Regular Expression" )); // true
 
     }
 
